25410679ba50d6d26a8587a37a82a66bb4de396b
aeratek <zack@akou.cc>
2013-08-29 21:22:46 -0400

    Update 1-Kinisi_Pre-Req_Install.sh
    Created comments for testing purposes and alternative build.

--- 1-Kinisi_Pre-Req_Install.sh
+++ 1-Kinisi_Pre-Req_Install.sh
@@ -3,18 +3,17 @@
 
 apt-get update -y
 apt-get upgrade -y
-/etc/init.d/networking restart
+#/etc/init.d/networking restart
 apt-get install openssh-server -y
-apt-get install software-properties-common -y
-apt-get update -y
-add-apt-repository ppa:chris-lea/node.js -y
-apt-get update -y
-apt-get install -y nodejs
-apt-get install -y npm
-apt-get install git -y
-apt-get install postgresql -y
-apt-get install postgresql-server-dev-9.1 -y
-apt-get install postgresql-contrib-9.1 -y
-
-apt-get install apt-show-versions -y
+#apt-get install software-properties-common -y
+#apt-get update -y
+#add-apt-repository ppa:chris-lea/node.js -y
+#apt-get update -y
+#apt-get install -y nodejs
+#apt-get install -y npm
+#apt-get install git -y
+#apt-get install postgresql -y
+#apt-get install postgresql-server-dev-9.1 -y
+#apt-get install postgresql-contrib-9.1 -y
+#apt-get install apt-show-versions -y
 
